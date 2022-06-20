5c340ae2001b84419ccc1c27bd812c5bd09bdbcf
Bram Vandenbussche <bram.vandenbussche@informat.be>
2022-06-20 21:58:57 +0200

    Initial commit

---
+++ Demo/Demo/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo;
+
+public class Calculator
+{
+    public double Value;
+
+    public double DoSum(double a, int b)
+    {
+        return a + b;
+    }
+
+    public void Display(string x)
+    {
+        Console.WriteLine(x);
+    }
+
+}
+
+public class Car
+{
+    public int NumberOfSeats;
+
+    public string Make;
+
+    public string Model;
+}
---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using Demo;
+
+Console.WriteLine("Hello, World!");
+
+
+var foo = new Calculator();
+
+
+foo.Value = foo.DoSum(2.5, 2);
+foo.Display(foo.Value.ToString());
+
+
+var carOne = new Car();
+carOne.NumberOfSeats = 5;
+carOne.Make = "Volkswagen";
+carOne.Model = "Caddy";
+
+Console.WriteLine(carOne);
+
+var carTwo = new Car();
+carTwo.NumberOfSeats = 7;
+carTwo.Make = "Volkswagen";
+carTwo.Model = "Caddy Maxi";
+
+Console.WriteLine(carTwo);
+
+Console.ReadLine();
